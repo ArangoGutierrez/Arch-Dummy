19b739ed40646cf2863a4175531c0bbd8cc02062
Carlos Eduardo Arango Gutierrez <carangog@redhat.com>
2020-03-26 13:48:22 -0400

    initial commit
    Signed-off-by: Carlos Eduardo Arango Gutierrez <carangog@redhat.com>

---
+++ cmd/dummy/main.go
@@ -0,0 +1,89 @@
+/*
+Copyright 2020 Red Hat
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/klauspost/cpuid"
+)
+
+// version returns the api version
+func version(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Dummy!\n"))
+}
+
+// cpu returns the cpu info
+func cpu(w http.ResponseWriter, r *http.Request) {
+	w.Write(
+		[]byte(fmt.Sprintf("{name:\"%s\",model:\"%d\",family:\"%d\"}\n", cpuid.CPU.BrandName, cpuid.CPU.Model, cpuid.CPU.Family)),
+	)
+}
+
+func main() {
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/version", version)
+	r.HandleFunc("/cpu", cpu)
+
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ArangoGutierrez/Arch-Dummy
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/klauspost/cpuid v1.2.3
+)
